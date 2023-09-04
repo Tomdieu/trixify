e3b78a72dd7bb4677ffdce8169210dd8b3adf8e2
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-09-04 09:55:36 +0000

    updated ccaat ap

--- backend/chats/admin.py
+++ backend/chats/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
+from .models import Chat,Conversation,GroupConversation,GroupMember,Message,Seen,Story,StoryViewer
+
+from polymorphic.admin import (
+    PolymorphicParentModelAdmin,
+    PolymorphicChildModelAdmin,
+    PolymorphicChildModelFilter,
+)
+
 # Register your models here.
+
+@admin.register(Chat)
+class ChatAdmin(PolymorphicParentModelAdmin):
+    list_display = ['is_group','created_at']
+    base_model = Chat
+    child_models = [
+        Conversation,
+        GroupConversation
+    ]
+    list_filter = (PolymorphicChildModelFilter,)
+
+
+class GroupMemberInline(admin.TabularInline):
+    model=GroupMember
+    extra = 1
+
+@admin.register(GroupConversation)
+class GroupConversationAdmin(admin.ModelAdmin):
+    list_display=('name', 'description','created_by' )
+
+    inlines = [
+        GroupMemberInline]
+
+@admin.register(GroupMember)
+class GroupMemberAdmin(admin.ModelAdmin):
+    list_display = ('user','group','is_active','is_admin','joined_on')
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display=['id']
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display=['conversation','sender','file','content','parent_message','created_at']
+
+@admin.register(Seen)
+class SeenAdmin(admin.ModelAdmin):
+    list_display=["id","message","user",'created_at']
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ['user','content','file','created_at']
+
+@admin.register(StoryViewer)
+class StoryAdmin(admin.ModelAdmin):
+    list_display=['id',"story","user","created_at"]
--- backend/chats/models.py
+++ backend/chats/models.py
@@ -10,25 +10,26 @@ from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
-
-class Conversation(PolymorphicModel):
-    """Conversation model"""
+class Chat(PolymorphicModel):
     is_group = models.BooleanField(default=False)
-    
-    users = models.ManyToManyField(User, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class GroupConversation(Conversation):
+
+class Conversation(Chat):
+    """Conversation model"""
+    users = models.ManyToManyField(User, related_name='conversations')
+
+class GroupConversation(Chat):
     name = models.CharField(max_length=100)
     group_icon = models.ImageField(upload_to='group_icons/', null=True, blank=True)
-    description = models.TextField(null=True,blanck=True)
+    description = models.TextField(null=True,blank=True)
+    created_by = models.ForeignKey(User, related_name='created_conversations', on_delete=models.CASCADE)
 
 
 class GroupMember(models.Model):
     user = models.ForeignKey(User, related_name='group_members', on_delete=models.CASCADE)
     group = models.ForeignKey(GroupConversation, related_name='group_members', on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, related_name='created_conversations', on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
@@ -45,10 +46,11 @@ class GroupMember(models.Model):
 
 class Message(models.Model):
     """Message Model"""
-    conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE)
+    conversation = models.ForeignKey(Chat, related_name='messages', on_delete=models.CASCADE)
     sender = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
     file = models.FileField(upload_to='files/', null=True, blank=True)
     content = models.TextField(null=True, blank=True)
+    parent_message = models.ForeignKey("self",on_delete=models.CASCADE,null=True,blank=True,related_name="replies")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -59,9 +61,29 @@ class Seen(models.Model):
     """Seen Model"""
     message = models.ForeignKey(Message, related_name='seen', on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='seen', on_delete=models.CASCADE)
-    seen = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f'{self.user} - {self.message} - {self.seen}'
+
+
+class Story(models.Model):
+    """Story Model"""
+    user = models.ForeignKey(User, related_name='stories', on_delete=models.CASCADE)
+    content = models.TextField(null=True,blank=True)
+    file = models.FileField(upload_to='stories/',null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
+    def __str__(self) -> str:
+        return f'{self.user} - Story'
+
+class StoryViewer(models.Model):
+    story = models.ForeignKey(Story,on_delete=models.CASCADE,related_name="views")
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='story_viewed')
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} view {self.story}"
