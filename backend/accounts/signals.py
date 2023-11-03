33777d8df3a0dfbf3f7a08925aec5954841127eb
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-11-03 20:53:35 +0100

    added accounts signal

---
+++ backend/accounts/signals.py
@@ -0,0 +1,15 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+from django.contrib.auth import get_user_model
+
+from .models import Profile
+
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
