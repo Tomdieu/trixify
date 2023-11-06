6f04740eb1be2a2bbf1779d2f89b402f0f821e2e
Tomdieu <ivantomdio@gmail.com>
2023-11-06 22:08:37 +0100

    added post serializers

---
+++ backend/posts/api/serializers.py
@@ -0,0 +1,82 @@
+from rest_framework import serializers
+
+from posts.models import Post,PostMedia,PostComment,PostReactions,CommentReactions
+
+from accounts.api.serializers import UserSerializer
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer(read_only=True,many=True)
+    class Meta:
+
+        model = Post
+        fields = ['id','title','content','created_by','updated','created_at']
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+
+        model = Post
+        fields = ['id','title','content','updated','created_at']
+
+    
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Post
+        fields = ['id','title','content','created_by','updated','created_at']
+
+class PostMediaCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = PostMedia
+        fields = ['file']
+
+class PostMediaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PostMedia
+        fields = ['file']
+
+class PostCommentCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PostComment
+        fields = ['parent','content','created_by','created_at']
+
+
+class PostCommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PostComment
+        fields = ['post','parent','content','created_by','created_at']
+
+class PostReactionCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PostReactions
+        fields = ['reaction','post']
+
+
+class PostReactionSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=True)
+    class Meta:
+        model = PostReactions
+        fields = ['reaction','post','user']
+
+
+class CommentReactionCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CommentReactions
+        fields = ['reaction','comment']
+
+
+class CommentReactionSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=True)
+    class Meta:
+        model = CommentReactions
+        fields = ['reaction','comment','user']
+
+
