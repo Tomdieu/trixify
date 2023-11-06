3fed2dedafeff77670dac82f4b0c2fa3587f8c76
Tomdieu <ivantomdio@gmail.com>
2023-11-06 22:07:19 +0100

    updated chat api views

--- backend/chats/api/views.py
+++ backend/chats/api/views.py
@@ -1,3 +1,7 @@
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication, BasicAuthentication, SessionAuthentication
+from rest_framework.decorators import action
+from rest_framework.decorators import authentication_classes, permission_classes, schema
 from rest_framework.mixins import (
     CreateModelMixin,
     ListModelMixin,
@@ -5,24 +9,9 @@ from rest_framework.mixins import (
     UpdateModelMixin,
     DestroyModelMixin,
 )
-from rest_framework.viewsets import GenericViewSet
-
-from .serializers import (
-    ChatSerializer,
-    MessageSerializer,
-    SeenSerializer,
-    ConversationSerializer,
-    ChatPolymorphicSerializer,
-    GroupConversationSerializer,
-    GroupMemberSerializer,
-    MessageTypeSerializer,
-    FileMessageSerializer,
-    TextMessageSerializer,
-    MessageReactionsSerializer
-)
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.viewsets import GenericViewSet
 
 from chats.models import (
     Chat,
@@ -31,14 +20,25 @@ from chats.models import (
     Conversation,
     GroupConversation,
     GroupMember,
-    MessageType,
-    FileMessage,
-    TextMessage,
-    LinkMessage, StoryReplyMessage,
     MessageReactions
 )
+from .serializers import (
+    MessageSerializer,
+    SeenSerializer,
+    ConversationSerializer,
+    ChatPolymorphicSerializer,
+    GroupConversationSerializer,
+    GroupMemberSerializer,
+    MessageReactionsSerializer,AddGroupMemberSerializer, JoinRemoveGroupMemberSerializer, CreateGroupConversationSerializer,
+    GroupConversationListSerializer,
+)
+
+# from drf_yasg.utils import swagger_auto_schema
+# from drf_yasg import openapi
 
 
+@authentication_classes([TokenAuthentication, BasicAuthentication, SessionAuthentication])
+@permission_classes([IsAuthenticated])
 class ChatViewSet(
     CreateModelMixin,
     ListModelMixin,
@@ -51,6 +51,8 @@ class ChatViewSet(
     serializer_class = ChatPolymorphicSerializer
 
 
+@authentication_classes([TokenAuthentication, BasicAuthentication, SessionAuthentication])
+@permission_classes([IsAuthenticated])
 class MessageViewSet(
     CreateModelMixin,
     ListModelMixin,
@@ -62,7 +64,18 @@ class MessageViewSet(
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
 
+    @action(methods=['GET', 'POST', 'DELETE'], detail=True, serializer_class=MessageReactionsSerializer)
+    def reactions(self, request, pk=None):
+        msg_obj = self.get_object()
 
+        reactions = MessageReactions.objects.filter(message_id=msg_obj.id)
+        serializer = MessageReactionsSerializer(reactions, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@authentication_classes([TokenAuthentication, BasicAuthentication, SessionAuthentication])
+@permission_classes([IsAuthenticated])
 class SeenViewSet(
     CreateModelMixin,
     ListModelMixin,
@@ -75,6 +88,8 @@ class SeenViewSet(
     serializer_class = SeenSerializer
 
 
+@authentication_classes([TokenAuthentication, BasicAuthentication, SessionAuthentication])
+@permission_classes([IsAuthenticated])
 class ConversationViewSet(
     CreateModelMixin,
     ListModelMixin,
@@ -87,6 +102,8 @@ class ConversationViewSet(
     serializer_class = ConversationSerializer
 
 
+@authentication_classes([TokenAuthentication, BasicAuthentication, SessionAuthentication])
+@permission_classes([IsAuthenticated])
 class GroupConversationViewSet(
     CreateModelMixin,
     ListModelMixin,
@@ -96,9 +113,115 @@ class GroupConversationViewSet(
     GenericViewSet,
 ):
     queryset = GroupConversation.objects.all()
-    serializer_class = GroupConversationSerializer
+
+    # serializer_class = GroupConversationSerializer
+
+    def get_serializer_class(self):
+
+        if self.action == "create":
+            return CreateGroupConversationSerializer
+        if self.action in ["list", "retrieve"]:
+            return GroupConversationListSerializer
+        elif self.action in ["update", "partial_update"]:
+            return GroupConversationSerializer
+        elif self.action == "messages":
+            return MessageSerializer
+        elif self.action == "members":
+            return GroupMemberSerializer
+        elif self.action in ["remove_member"]:
+            return JoinRemoveGroupMemberSerializer
+        elif self.action in  ["add_member"]:
+            return AddGroupMemberSerializer
+        elif self.action in  ["join"]:
+            return None
+
+        return GroupConversationSerializer
+    
+    
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+   
+    @action(methods=['GET', 'POST'], detail=True)
+    def messages(self, request, pk=None):
+
+        group_obj = self.get_object()
+
+        if request.method == "GET":
+
+            serializer = MessageSerializer(Message.objects.filter(conversation=group_obj), many=True)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        elif request.method == "POST":
+
+            serializer = MessageSerializer(data=request.data)
+
+            serializer.is_valid(raise_exception=True)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, pk=None):
+        group_obj: GroupConversation = self.get_object()
+
+        if group_obj.created_by == self.request.user:
+            group_obj.delete()
+
+            return Response({"message": "group deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
+        return Response({"detail": "You are not authorized to perform this action"},
+                        status=status.HTTP_401_UNAUTHORIZED)
 
 
+    @action(methods=['GET'], detail=True)
+    def members(self, request, pk=None):
+
+        group_obj = self.get_object()
+        group_members = GroupMember.objects.filter(group_id=group_obj)
+        serializer = GroupMemberSerializer(group_members, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    @action(methods=['POST'], detail=True, url_path='add-member')
+    def add_member(self, request, pk=None):
+
+        group_obj: GroupConversation = self.get_object()
+
+        serializer = JoinRemoveGroupMemberSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        for user in serializer.validated_data['user_id']:
+            group_obj.add_group_member(serializer.validated_data['user_id'])
+
+            serializer = self.get_serializer(group_obj)
+
+        return Response({"message":"Members added successfully"}, status=status.HTTP_201_CREATED)
+
+    @action(methods=['POST'], detail=True)
+    def join(self, request, pk=None):
+
+        group_obj: GroupConversation = self.get_object()
+
+        group_obj.add_group_member(request.user)
+
+        serializer = self.get_serializer(group_obj)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(methods=['POST'], detail=True,
+            url_path='remove-member')
+    def remove_member(self, request, pk=None):
+        group_obj: GroupConversation = self.get_object()
+        serializer = JoinRemoveGroupMemberSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        group_obj.remove_group_member(request.data.get('user_id'))
+
+        return Response(status=status.HTTP_200_OK)
+
+
+@authentication_classes([TokenAuthentication, BasicAuthentication, SessionAuthentication])
+@permission_classes([IsAuthenticated])
 class GroupMemberViewSet(
     CreateModelMixin,
     ListModelMixin,
@@ -111,12 +234,13 @@ class GroupMemberViewSet(
     serializer_class = GroupMemberSerializer
 
 
+@authentication_classes([TokenAuthentication, BasicAuthentication, SessionAuthentication])
+@permission_classes([IsAuthenticated])
 class MessageReactionViewSet(CreateModelMixin,
                              ListModelMixin,
                              RetrieveModelMixin,
                              UpdateModelMixin,
                              DestroyModelMixin,
                              GenericViewSet, ):
-
     queryset = MessageReactions.objects.all()
     serializer_class = MessageReactionsSerializer
