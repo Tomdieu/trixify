54e3196fa99c97cf9c08ee0ba157a16542cc75c7
Tomdieu <ivantomdio@gmail.com>
2023-11-06 22:07:29 +0100

    updated chat api urls

--- backend/chats/api/urls.py
+++ backend/chats/api/urls.py
@@ -5,10 +5,10 @@ from . import views
 router = DefaultRouter()
 router.register(r"chats", views.ChatViewSet)
 router.register(r"messages", views.MessageViewSet)
-router.register(r"saw", views.SeenViewSet)
+# router.register(r"seen-by", views.SeenViewSet)
 router.register(r"conversations", views.ConversationViewSet)
-router.register(r"group-conversations", views.GroupConversationViewSet)
-router.register(r"group-members", views.GroupMemberViewSet)
+router.register(r"groups", views.GroupConversationViewSet)
+# router.register(r"group-members", views.GroupMemberViewSet)
 router.register("message-reactions",views.MessageReactionViewSet,basename="message-reactions")
 
 
