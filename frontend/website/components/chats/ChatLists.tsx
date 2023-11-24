8834837a88232a5ec0b30a0fd34199710fef3827
Tomdieu <ivantomdio@gmail.com>
2023-11-24 17:41:38 +0100

    added chat list component

---
+++ frontend/website/components/chats/ChatLists.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import ChatItem from "@/components/chats/ChatItem";
+import {ConversationType, GroupType} from "@/types";
+
+type ChatListsProps = {
+    chats:ConversationType[]|GroupType[]
+}
+
+export default function ChatLists({chats}:ChatListsProps) {
+
+    return (
+        <div className={"h-full flex-1 relative overflow-y-auto custom-scrollbar"}>
+
+            {chats.map((chat)=><ChatItem key={chat.id} chat={chat}/>)}
+        </div>
+    );
+}
