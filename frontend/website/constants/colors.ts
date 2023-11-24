0d0a6985755a97e9ce68e6aeae056c9669e3235a
Tomdieu <ivantomdio@gmail.com>
2023-11-24 17:32:24 +0100

    added colors contants

---
+++ frontend/website/constants/colors.ts
@@ -0,0 +1,28 @@
+export const sideBarColors = {
+    iconColor:"#9098A4",
+    activeColor:"#2889FF",
+    activeShadowColor:"#87B6FF"
+}
+
+export const colorCombinations = [
+    { bg: 'bg-blue-500', text: 'text-white' },
+    { bg: 'bg-green-500', text: 'text-white' },
+    { bg: 'bg-yellow-500', text: 'text-black' },
+    { bg: 'bg-purple-500', text: 'text-white' },
+    { bg: 'bg-indigo-500', text: 'text-white' },
+    { bg: 'bg-pink-500', text: 'text-white' },
+    { bg: 'bg-red-500', text: 'text-white' },
+    { bg: 'bg-teal-500', text: 'text-white' },
+    { bg: 'bg-orange-500', text: 'text-black' },
+    { bg: 'bg-gray-500', text: 'text-white' },
+    { bg: 'bg-blue-700', text: 'text-white' },
+    { bg: 'bg-green-700', text: 'text-white' },
+    { bg: 'bg-yellow-700', text: 'text-white' },
+    { bg: 'bg-purple-700', text: 'text-white' },
+    { bg: 'bg-indigo-700', text: 'text-white' },
+    { bg: 'bg-pink-700', text: 'text-white' },
+    { bg: 'bg-red-700', text: 'text-white' },
+    { bg: 'bg-teal-700', text: 'text-white' },
+    { bg: 'bg-orange-700', text: 'text-white' },
+    { bg: 'bg-gray-700', text: 'text-white' },
+];
