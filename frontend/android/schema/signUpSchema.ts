1336a5b1aa2b6c489822e0730a0d7b77204a7fd7
Tomdieu <ivantomdio@gmail.com>
2023-11-24 16:56:24 +0100

    added schema

---
+++ frontend/android/schema/signUpSchema.ts
@@ -0,0 +1,23 @@
+import { z } from "zod"
+
+
+export const signUpSchema = z.object({
+    email: z.string().email().refine(async (email) => {
+        const res = await fetch(process.env.EXPO_PUBLIC_BACKEND_URL + "/api/accounts/check_email/?email=" + email)
+        const jsonData = await res.json()
+        const { exists } = jsonData
+        return exists !== true
+    }, { message: "Email already exists" }),
+    username: z.string().min(5, { message: "username should be at least 5 characters" }).max(100).refine(async (data) => {
+        const res = await fetch(process.env.EXPO_PUBLIC_BACKEND_URL + "/api/accounts/check_username/?username=" + data)
+        const jsonData = await res.json()
+        const { exists } = jsonData
+        return exists !== true
+    }, { message: "Username already exists" }),
+    password: z.string().min(8).max(100),
+    first_name: z.string().optional(),
+    last_name: z.string().optional(),
+    phone_number: z.string().min(9).max(20),
+})
+
+export type signUpSchemaType = z.infer<typeof signUpSchema>
