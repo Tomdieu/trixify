2aedb4fe3f257e79045bede86b9bd3c566c2dcd0
Tomdieu <ivantomdio@gmail.com>
2023-11-24 16:48:13 +0100

    updated contact admin

--- backend/contact/admin.py
+++ backend/contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Contact
+
+
 # Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'phone_number']
