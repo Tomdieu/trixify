b159821779952756265a1a893b07e19b34d4c236
Tomdieu <ivantomdio@gmail.com>
2023-11-24 16:48:23 +0100

    updated contact models

--- backend/contact/models.py
+++ backend/contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+
 # Create your models here.
+
+User = get_user_model()
+
+
+class Contact(models.Model):
+    user = models.ForeignKey(User, related_name='contacts', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name} - {self.phone_number}"
