c408ff4ba769c9d275eb0534b3ed6a59b4d346c5
Tomdieu <ivantomdio@gmail.com>
2023-11-24 18:06:05 +0100

    updated

--- frontend/website/tailwind.config.ts
+++ frontend/website/tailwind.config.ts
@@ -16,6 +16,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        'poppins': ['var(--font-poppins)', 'sans-serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
