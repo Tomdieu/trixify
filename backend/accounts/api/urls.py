bf6ddeb965eb6f91b6aacaedd348d4483fc63186
Tomdieu <ivantomdio@gmail.com>
2023-11-24 16:44:42 +0100

    updated accounts urls

--- backend/accounts/api/urls.py
+++ backend/accounts/api/urls.py
@@ -10,9 +10,10 @@ router.register("profile", views.UserProfileViewSet, basename="profile")
 router.register("login", views.LoginViewSet, basename="login")
 router.register("register", views.RegisterViewSet, basename="register")
 router.register("user-links", views.UserSocialLinksViewSet, basename='user-links')
-# router.register("logout",views.LogoutView)
 
 urlpatterns = [
+    path("check_username/",views.UsernameExistsAPIView.as_view()),
+    path("check_email/",views.EmailExistsAPIView.as_view()),
     path("logout/", views.LogoutView.as_view())
 ]
 
