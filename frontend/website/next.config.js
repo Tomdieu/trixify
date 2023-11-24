025c6b187762a9d8bb2d13c9c15f525e6dca6f6a
Tomdieu <ivantomdio@gmail.com>
2023-11-24 18:05:06 +0100

    enable localhost

--- frontend/website/next.config.js
+++ frontend/website/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'localhost',
+            },
+        ],
+    },
+    
+}
 
 module.exports = nextConfig
