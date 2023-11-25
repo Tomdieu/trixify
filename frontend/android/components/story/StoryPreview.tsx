c259972b014784ddfe3a06bf2adaa68ec5ee4df5
Tomdieu <ivantomdio@gmail.com>
2023-11-25 01:26:42 +0100

    added story preview

---
+++ frontend/android/components/story/StoryPreview.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { Avatar } from '@react-native-material/core'
+import { UserType } from '@/types'
+
+type Props = {
+    user: UserType,
+    isMine?: boolean
+}
+
+const StoryPreview = ({ user, isMine=false }: Props) => {
+    return (
+        <TouchableOpacity>
+            <View className='mx-1'>
+                <View className='relative'>
+                    {isMine && (
+                        <View className='absolute animate-pin bottom-0 right-1 bg-yellow-400 z-50 w-5 h-5 rounded-full flex items-center justify-center'>
+                        <Text className='text-sm'>+</Text>
+                    </View>
+                    )}
+                    <Avatar image={{ uri: user.avatar }} color='#2987FF' labelStyle={{ color: "#fff", fontSize: 20 }} label={user.username} />
+                </View>
+                <Text className='text-xs text-gray-500'>{isMine ? "Your Story":user.username}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+export default StoryPreview
+
+const styles = StyleSheet.create({})
