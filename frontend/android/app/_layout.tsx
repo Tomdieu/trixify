5142229a76f0dc312bfe5d001f3ad7b582dc1304
Tomdieu <ivantomdio@gmail.com>
2023-11-25 01:30:38 +0100

    feat : updated global layout

--- frontend/android/app/_layout.tsx
+++ frontend/android/app/_layout.tsx
@@ -1,15 +1,36 @@
 import React, { useEffect } from "react";
 import { Slot, useRouter } from "expo-router";
 import { useAuth } from "@/hooks/userAuth";
+import { useFonts } from 'expo-font';
 
-export default function () {
+// import * as SplashScreen from 'expo-splash-screen';
+import { SafeAreaView, StatusBar, Text } from "react-native";
+import ReactQueryProvider from "@/provider/ReactQueryProvider";
+
+// SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
   const { isLoggedIn } = useAuth();
   const router = useRouter();
+  // const [fontsLoaded] = useFonts({
+  //   'Poppins-Regular': require('../assets/fonts/poppins/Poppins-Regular.ttf'),
+  //   'Poppins-Bold': require('../assets/fonts/poppins/Poppins-Bold.ttf'),
+  //   'Poppins-SemiBold': require('../assets/fonts/poppins/Poppins-SemiBold.ttf'),
+  //   'Poppins-Medium': require('../assets/fonts/poppins/Poppins-Medium.ttf'),
+  //   'Poppins-Light': require('../assets/fonts/poppins/Poppins-Light.ttf'),
+  // });
+
+
+  // if (!fontsLoaded) {
+  //   return <Text>Loading Fonts</Text>;
+  // }
 
   useEffect(() => {
     if (isLoggedIn) {
       router.push("chats/");
     }
   },[]);
-  return <Slot />;
+  return <ReactQueryProvider>
+    <Slot />
+    </ReactQueryProvider>
 }
