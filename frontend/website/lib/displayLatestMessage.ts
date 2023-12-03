b54abecdaa744595099e0781f0a4ac1729970e0e
Tomdieu <ivantomdio@gmail.com>
2023-12-04 00:57:19 +0100

    feat : added display latest message function

---
+++ frontend/website/lib/displayLatestMessage.ts
@@ -0,0 +1,45 @@
+import { ChatMessageType, ChatType, ConversationType, FileMessageType, GroupType, TextMessageType } from "@/types";
+
+export default function displayLatestMessage(chat:ChatType){
+
+    if(chat.is_group){
+        const group = chat as GroupType;
+        if(group.latest_message){
+            const message = group.latest_message as ChatMessageType;
+            
+            if(message.content.resourcetype=="TextMessage"){
+                const textMessage = message.content as TextMessageType;
+                return message.sender.username+": "+textMessage.text;
+            }
+            else if(message.content.resourcetype=="FileMessage"){
+                const fileMessage = message.content as FileMessageType;
+                return message.sender.username+": send a file";
+            }
+            else if(message.content.resourcetype=="StoryReplyMessage"){
+                const storyReplyMessage = message.content as FileMessageType;
+                return message.sender.username+": reply to a story";
+            }
+        }
+    }
+
+    const conversation = chat as ConversationType;
+    if(conversation){
+        if(conversation.latest_message){
+            const message = conversation.latest_message as ChatMessageType;
+            
+            if(message.content.resourcetype=="TextMessage"){
+                const textMessage = message.content as TextMessageType;
+                return message.sender.username+": "+textMessage.text;
+            }
+            else if(message.content.resourcetype=="FileMessage"){
+                const fileMessage = message.content as FileMessageType;
+                return message.sender.username+": send a file";
+            }
+            else if(message.content.resourcetype=="StoryReplyMessage"){
+                const storyReplyMessage = message.content as FileMessageType;
+                return message.sender.username+": reply to a story";
+            }
+        }
+    }
+
+}
