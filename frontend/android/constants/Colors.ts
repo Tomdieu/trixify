51d6137ce82b287e41f67c6fa24308a268ec1168
Tomdieu <ivantomdio@gmail.com>
2023-12-15 22:55:32 +0100

    feat : added constants

--- frontend/android/constants/Colors.ts
+++ frontend/android/constants/Colors.ts
@@ -57,3 +57,56 @@ export const textColors = [
   '#008080',
   '#000080',
 ]
+
+
+export const twBgColors = [
+
+  "bg-blue-400",
+  "bg-blue-500",
+  "bg-blue-600",
+  "bg-blue-700",
+  "bg-blue-800",
+  "bg-blue-900",
+
+  "bg-orange-500",
+  "bg-orange-600",
+  "bg-orange-700",
+  "bg-orange-800",
+  "bg-orange-900",
+
+  "bg-green-500",
+  "bg-green-600",
+  "bg-green-700",
+  "bg-green-800",
+  "bg-green-900",
+
+  "bg-red-400",
+  "bg-red-500",
+  "bg-red-600",
+  "bg-red-700",
+  "bg-red-800",
+  "bg-red-900",
+
+  "bg-pink-500",
+  "bg-pink-600",
+  "bg-pink-700",
+  "bg-pink-800",
+  "bg-pink-900",
+
+  "bg-purple-500",
+  "bg-purple-600",
+  "bg-purple-700",
+  "bg-purple-800",
+  "bg-purple-900",
+
+  "bg-yellow-500",
+  "bg-yellow-600",
+  "bg-yellow-700",
+  "bg-yellow-800",
+  "bg-yellow-900"
+];
+
+
+export function randomBgColor() {
+  return twBgColors[Math.floor(Math.random() * twBgColors.length)];
+}
