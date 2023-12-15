99531ce421863b8af91ded5cbe4846c5963523ca
Tomdieu <ivantomdio@gmail.com>
2023-12-15 23:03:48 +0100

    feat : updated

--- frontend/website/app/page.tsx
+++ frontend/website/app/page.tsx
@@ -1,14 +1,14 @@
 import Sidebar from "@/components/sidebar/Sidebar"
 
 export default async function Home() {
-  return (
-    <div className="bg-white h-screen flex w-full">
-      <div className="w-1.5/12">
-      <Sidebar/>
+    return (
+        <div className="bg-white h-screen flex w-full">
+            <div className="w-2/12">
+                <Sidebar/>
+            </div>
+            <div className="w-10/12">
+                <h1 className="text-5xl">Hello</h1>
+            </div>
         </div>
-      <div className="w-10.5/12">
-      <h1 className="text-5xl">Hello</h1>
-        </div>
-    </div>
-  )
+    )
 }
