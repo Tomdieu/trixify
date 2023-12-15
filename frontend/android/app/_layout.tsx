dee7deb31adf3770e418181f13074f38869f9d27
Tomdieu <ivantomdio@gmail.com>
2023-12-15 22:58:06 +0100

    updated root layout

--- frontend/android/app/_layout.tsx
+++ frontend/android/app/_layout.tsx
@@ -1,36 +1,49 @@
 import React, { useEffect } from "react";
-import { Slot, useRouter } from "expo-router";
+import { Slot, useRouter, SplashScreen } from "expo-router";
 import { useAuth } from "@/hooks/userAuth";
 import { useFonts } from 'expo-font';
-
-// import * as SplashScreen from 'expo-splash-screen';
-import { SafeAreaView, StatusBar, Text } from "react-native";
+import * as Font from 'expo-font';
+import { ThemeProvider, DarkTheme,DefaultTheme } from '@react-navigation/native';
 import ReactQueryProvider from "@/provider/ReactQueryProvider";
 
-// SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const { isLoggedIn } = useAuth();
   const router = useRouter();
-  // const [fontsLoaded] = useFonts({
-  //   'Poppins-Regular': require('../assets/fonts/poppins/Poppins-Regular.ttf'),
-  //   'Poppins-Bold': require('../assets/fonts/poppins/Poppins-Bold.ttf'),
-  //   'Poppins-SemiBold': require('../assets/fonts/poppins/Poppins-SemiBold.ttf'),
-  //   'Poppins-Medium': require('../assets/fonts/poppins/Poppins-Medium.ttf'),
-  //   'Poppins-Light': require('../assets/fonts/poppins/Poppins-Light.ttf'),
-  // });
+
+  const loadFont = async () => {
+    return await Font.loadAsync({
+      'poppins-regular': require('@/assets/fonts/poppins/Poppins-Regular.ttf'),
+      'poppins-bold': require('@/assets/fonts/poppins/Poppins-Bold.ttf'),
+      'poppins-medium': require('@/assets/fonts/poppins/Poppins-Medium.ttf'),
+      'poppins-light': require('@/assets/fonts/poppins/Poppins-Light.ttf'),
+      'poppins-semibold': require('@/assets/fonts/poppins/Poppins-SemiBold.ttf'),
+      'poppins-thin': require('@/assets/fonts/poppins/Poppins-Thin.ttf'),
+      'poppins-extralight': require('@/assets/fonts/poppins/Poppins-ExtraLight.ttf'),
+      'poppins-extrabold': require('@/assets/fonts/poppins/Poppins-ExtraBold.ttf'),
+      'poppins-black': require('@/assets/fonts/poppins/Poppins-Black.ttf'),
+    })
+  }
+
+  useEffect(() => { 
+    
+    (async()=>{
+      await loadFont()
+      SplashScreen.hideAsync();
+      setTimeout(() => {
+        if (isLoggedIn) {
+          router.push("chats/");
+        }
+      },2000)
+    })()
+   }, [isLoggedIn])
 
 
-  // if (!fontsLoaded) {
-  //   return <Text>Loading Fonts</Text>;
-  // }
+  return (<ReactQueryProvider>
+    <ThemeProvider value={DefaultTheme}>
 
-  useEffect(() => {
-    if (isLoggedIn) {
-      router.push("chats/");
-    }
-  },[]);
-  return <ReactQueryProvider>
-    <Slot />
-    </ReactQueryProvider>
+      <Slot />
+    </ThemeProvider>
+  </ReactQueryProvider>)
 }
