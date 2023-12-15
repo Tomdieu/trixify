1b6a170f857cdfbc94aaacbe2580a40849a52754
Tomdieu <ivantomdio@gmail.com>
2023-12-15 23:05:01 +0100

    added react framer  motions components

---
+++ frontend/website/components/motion/index.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import {motion} from "framer-motion"
+
+export const MotionDiv = motion.div;
+export const MotionText = motion.text;
+export const MotionView = motion.view;
+export const MotionButton =  motion.button;
